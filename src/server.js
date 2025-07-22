f1fd33be1124a7d2bf529219acf6e01affa03408
Hick <124798752+hick-hpe@users.noreply.github.com>
2025-07-22 12:31:40 -0300

    fix(src/server.js): ajuste do frontend

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, '../views')));
 
 // Rota principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 // Arquivos estáticos
